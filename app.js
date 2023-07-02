4d00e9f12998f81251577e921bd8f7e5bc17c8ea
TonyBalde18 <antonio.balde@ubi.pt>
2023-07-02 18:47:22 +0100

    final animations

--- app.js
+++ app.js
@@ -26,9 +26,17 @@ form.addEventListener("submit", (e) => {
     }
   });
 
-   // show the result
-   scrollTo(0, 0);
-   result.querySelector('span').textContent = `${score}%`;
-   result.classList.remove('d-none');
- 
- });
+  // show the result
+  scrollTo(0, 0);
+  result.classList.remove("d-none");
+
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector("span").textContent = `${output}%`;
+    if (output === score) {
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
+});
